d597105bc9460a0e92b3d4b3c0764af49fdda77a
Daniel Khrapko <55002392+khrapkodaniel@users.noreply.github.com>
2021-02-23 11:29:26 -0500

    Update Roman_to_Integer.java

--- src/easy/Roman_to_Integer.java
+++ src/easy/Roman_to_Integer.java
@@ -1,9 +1,30 @@
 package easy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Roman_to_Integer {
 	public static void main(String[] args) {
-		
+		System.out.println(romanToInt("VI"));
 	}
 	
-	
+	private static int romanToInt(String s) {
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		
+		map.put("I", 1);
+		map.put("V", 5);
+		map.put("X", 10);
+		map.put("L", 50);
+		map.put("C", 100);
+		map.put("D", 500);
+		map.put("M", 1000);
+		
+		int ans = 0;
+		
+		for(int i = s.length(); i > 0; i--) {
+			ans += map.get(s.substring(i - 1, i));
+		}
+		
+		return ans;
+	}
 }
