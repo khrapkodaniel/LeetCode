a4f2135a4672a5056c2bf89ba2511e4ca9746931
Daniel Khrapko <55002392+khrapkodaniel@users.noreply.github.com>
2021-02-23 11:46:32 -0500

    Update Roman_to_Integer.java

--- src/easy/Roman_to_Integer.java
+++ src/easy/Roman_to_Integer.java
@@ -9,20 +9,22 @@ public class Roman_to_Integer {
 	}
 	
 	private static int romanToInt(String s) {
-		Map<String, Integer> map = new HashMap<String, Integer>();
+		Map<Character, Integer> map = new HashMap<Character, Integer>();
 		
-		map.put("I", 1);
-		map.put("V", 5);
-		map.put("X", 10);
-		map.put("L", 50);
-		map.put("C", 100);
-		map.put("D", 500);
-		map.put("M", 1000);
+		map.put('I', 1);
+		map.put('V', 5);
+		map.put('X', 10);
+		map.put('L', 50);
+		map.put('C', 100);
+		map.put('D', 500);
+		map.put('M', 1000);
 		
 		int ans = 0;
 		
-		for(int i = s.length(); i > 0; i--) {
-			ans += map.get(s.substring(i - 1, i));
+		for (int i = 0; i < s.length(); i++) {
+			if (i < s.length() - 1 && map.get(s.charAt(i)) < map.get(s.charAt(i + 1)))
+				ans -= map.get(s.charAt(i));
+			else ans += map.get(s.charAt(i));
 		}
 		
 		return ans;
