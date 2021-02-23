009573bad0d35fe5f63fa29dff1bbf81bab5c67c
Daniel Khrapko <55002392+khrapkodaniel@users.noreply.github.com>
2021-02-22 22:53:31 -0500

    Create Palindrome_Number.java

---
+++ src/easy/Palindrome_Number.java
@@ -0,0 +1,34 @@
+package easy;
+
+public class Palindrome_Number {
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+	private static boolean isPalindrome(int x) {
+		if (x < 0) return false;
+		
+		return reverse(x) == x;
+	}
+	
+	private static int reverse(int x) {
+		int reversed = 0;
+		
+		while ( x != 0 ) {
+			int digit = x % 10;
+			x /= 10;
+			
+			if(reversed > Integer.MAX_VALUE / 10 || (reversed == Integer.MAX_VALUE / 10 && digit > 7))
+				return 0;
+			
+			if(reversed < Integer.MIN_VALUE / 10 || (reversed == Integer.MIN_VALUE / 10 && digit < -8))
+				return 0;
+			
+			reversed = reversed * 10 + digit;
+		}
+		
+		return reversed;
+	}
+}
