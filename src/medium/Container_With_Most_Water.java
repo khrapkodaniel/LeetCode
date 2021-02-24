33141f2faaa56536eb97106db98af11d76bf0916
Daniel Khrapko <55002392+khrapkodaniel@users.noreply.github.com>
2021-02-24 11:23:11 -0500

    Update Container_With_Most_Water.java

--- src/medium/Container_With_Most_Water.java
+++ src/medium/Container_With_Most_Water.java
@@ -6,6 +6,10 @@ public class Container_With_Most_Water {
 	}
 	
 	public int maxArea(int[] height) {
-		
+        int area = 0;
+        for(int i = 0; i < height.length; i++)
+            for(int j = i + 1; j < height.length; j++) 
+                area = Math.max(area, Math.min(height[i], height[j]) * (j - i));
+        return area;
 	}
 }
